c4d3d7b5837e4fc0debf088f7c4c748f2e9bb794
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-12-06 20:03:10 -0500

    adds base project

---
+++ RelationChips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RelationChips
+//
+//  Created by Chris Gonzales on 12/6/14.
+//  Copyright (c) 2014 GNZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RelationChips/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RelationChips
+//
+//  Created by Chris Gonzales on 12/6/14.
+//  Copyright (c) 2014 GNZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
